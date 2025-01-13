0f70e6ab37cd94e3b3d664c5bd2cd0e0a078f44e
Kaleb <kalebzambrano@outlook.com>
2025-01-12 20:40:34 -0500

    Pasantia dinamicPath and new layout

---
+++ src/components/scripts/pasantias.js
@@ -0,0 +1,48 @@
+const pasantias = [
+    {
+        id: "Peru-Chile-2019",
+        title: "Perú - Chile 2019",
+        date: "Febrero 2019",
+        description: "Hola desde Chile",
+        images: [
+            "img-1", "img-2"
+        ]
+    },
+    {
+        id: "Peru-Costa-Rica-2019",
+        title: "Perú - Costa Rica 2019",
+        date: "Febrero 2019",
+        description: "Hola desde Costa Rica",
+        images: [
+            "img-1", "img-2"
+        ]
+    },
+    {
+        id: "Peru-Mexico-2020",
+        title: "Perú - México 2020",
+        date: "Febrero 2019",
+        description: "Hola desde Costa Rica",
+        images: [
+            "img-1", "img-2"
+        ]
+    },
+    {
+        id: "Peru-Colombia-2022",
+        title: "Perú - Colombia 2022",
+        date: "Febrero 2019",
+        description: "Hola desde Costa Rica",
+        images: [
+            "img-1", "img-2"
+        ]
+    },
+    {
+        id: "Peru-Costa-Rica-2023",
+        title: "Perú - Costa Rica 2023",
+        date: "Febrero 2019",
+        description: "Hola desde Costa Rica",
+        images: [
+            "img-1", "img-2"
+        ]
+    }
+]
+export default pasantias;
